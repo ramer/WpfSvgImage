543fa0f4fe5d1a0e363950e7f095a6569aa56a61
ramer <ramer.mobile@gmail.com>
2019-12-13 01:30:31 +0300

    added recurse reader

--- SVGReader/SVGImage.vb
+++ SVGReader/SVGImage.vb
@@ -101,11 +101,11 @@ Public Class SvgImage
 
             If Not File.Exists(path) Then
                 Using stream = Windows.Application.GetResourceStream(uri).Stream
-                    UpdateSvgData(stream)
+                    UpdateDataFromStream(stream)
                 End Using
             Else
                 Using stream = New FileStream(path, FileMode.Open)
-                    UpdateSvgData(stream)
+                    UpdateDataFromStream(stream)
                 End Using
             End If
         Catch ex As Exception
@@ -113,115 +113,144 @@ Public Class SvgImage
         End Try
     End Sub
 
-    Private Sub UpdateSvgData(stream As Stream)
+    Private Sub UpdateDataFromStream(stream As Stream)
         Dim readersettings As New XmlReaderSettings With {.DtdProcessing = DtdProcessing.Ignore} ' .XmlResolver = Nothing,
-        Dim geometrygroup As New GeometryGroup With {.FillRule = FillRule.Nonzero}
-
-        Dim gotwidth As Boolean
-        Dim newwidth As Double
-        Dim gotheight As Boolean
-        Dim newheight As Double
-        Dim gotviewbox As Boolean
-        Dim newviewbox As Rect
 
         Using reader = XmlReader.Create(stream, readersettings)
 
-            While reader.Read
-                If Not reader.NodeType = XmlNodeType.Element Then Continue While
+            reader.MoveToContent()
 
-                If reader.Name.ToLower = "svg" Then
-                    gotwidth = GetDoubleAttribute(reader, "width", newwidth)
-                    gotheight = GetDoubleAttribute(reader, "height", newheight)
-                    gotviewbox = GetRectAttribute(reader, "viewBox", newviewbox)
-                End If
+            Dim node = XNode.ReadFrom(reader)
+            If node.NodeType = XmlNodeType.Element Then Data = ReadSvgNode(node)
 
-                If reader.Name.ToLower = "path" Then
-                    Try
-                        Dim attr = reader.GetAttribute("d")
-                        If Not String.IsNullOrEmpty(attr) Then geometrygroup.Children.Add(Geometry.Parse(attr))
-                    Catch
-                    End Try
-                End If
-
-                If reader.Name.ToLower = "line" Then
-                    Dim x1, y1, x2, y2 As Double
-                    If GetDoubleAttribute(reader, "x1", x1) AndAlso
-                        GetDoubleAttribute(reader, "y1", y1) AndAlso
-                        GetDoubleAttribute(reader, "x2", x2) AndAlso
-                        GetDoubleAttribute(reader, "y2", y2) Then geometrygroup.Children.Add(New LineGeometry(New Point(x1, y2), New Point(x2, y2)))
-                End If
-
-                If reader.Name.ToLower = "rect" Then
-                    Dim x, y, w, h As Double
-                    If GetDoubleAttribute(reader, "x", x) AndAlso
-                        GetDoubleAttribute(reader, "y", y) AndAlso
-                        GetDoubleAttribute(reader, "width", w) AndAlso
-                        GetDoubleAttribute(reader, "height", h) Then geometrygroup.Children.Add(New RectangleGeometry(New Rect(x, y, w, h)))
-                End If
-
-                If reader.Name.ToLower = "ellipse" Then
-                    Dim cx, cy, rx, ry As Double
-                    If GetDoubleAttribute(reader, "cx", cx) AndAlso
-                        GetDoubleAttribute(reader, "cy", cy) AndAlso
-                        GetDoubleAttribute(reader, "rx", rx) AndAlso
-                        GetDoubleAttribute(reader, "ry", ry) Then geometrygroup.Children.Add(New EllipseGeometry(New Point(cx, cy), rx, ry))
-                End If
-
-                If reader.Name.ToLower = "circle" Then
-                    Dim cx, cy, r As Double
-                    If GetDoubleAttribute(reader, "cx", cx) AndAlso
-                        GetDoubleAttribute(reader, "cy", cy) AndAlso
-                        GetDoubleAttribute(reader, "r", r) Then geometrygroup.Children.Add(New EllipseGeometry(New Point(cx, cy), r, r))
-                End If
-
-                If reader.Name.ToLower = "polyline" Then
-                    Try
-                        Dim polyline As New Polyline
-                        polyline.Points = PointCollection.Parse(reader.GetAttribute("points"))
-                        polyline.Measure(ViewBox.Size)
-                        polyline.Arrange(ViewBox)
-                        geometrygroup.Children.Add(polyline.RenderedGeometry)
-                    Catch
-                    End Try
-                End If
-
-                If reader.Name.ToLower = "polygon" Then
-                    Try
-                        Dim polygon As New Polygon
-                        polygon.Points = PointCollection.Parse(reader.GetAttribute("points"))
-                        polygon.Measure(ViewBox.Size)
-                        polygon.Arrange(ViewBox)
-                        geometrygroup.Children.Add(polygon.RenderedGeometry)
-                    Catch
-                    End Try
-                End If
-            End While
         End Using
-
-        Data = geometrygroup
-        Clip = If(gotviewbox, New RectangleGeometry(newviewbox), Nothing)
-        Width = If(gotviewbox, newviewbox.Right, If(gotwidth, newwidth, Double.NaN))
-        Height = If(gotviewbox, newviewbox.Bottom, If(gotheight, newheight, Double.NaN))
-        ViewBox = If(gotviewbox, newviewbox, Nothing)
-        TranslateTransform.X = If(gotviewbox, -_viewbox.X, 0)
-        TranslateTransform.Y = If(gotviewbox, -_viewbox.Y, 0)
-        ScaleTransform.ScaleX = If(gotviewbox, _viewbox.Right / _viewbox.Width, 1)
-        ScaleTransform.ScaleY = If(gotviewbox, _viewbox.Bottom / _viewbox.Height, 1)
     End Sub
 
-    Private Function GetDoubleAttribute(reader As XmlReader, attr As String, ByRef d As Double) As Boolean
-        Dim strvalue As String = reader.GetAttribute(attr)
-        If String.IsNullOrEmpty(strvalue) Then Return False
-        strvalue = Regex.Replace(strvalue, "[^0-9.-]", "")
-        Return Double.TryParse(strvalue, d)
+    Private Function ReadSvgNode(ByVal node As XElement) As GeometryGroup
+        Dim result As New GeometryGroup With {.FillRule = FillRule.Nonzero}
+
+        If node.Name.LocalName = "svg" Then
+            Dim gotwidth As Boolean
+            Dim newwidth As Double
+            Dim gotheight As Boolean
+            Dim newheight As Double
+            Dim gotviewbox As Boolean
+            Dim newviewbox As Rect
+
+            For Each attr In node.Attributes
+                If attr.Name.LocalName = "width" Then gotwidth = GetDoubleAttribute(attr.Value, newwidth)
+                If attr.Name.LocalName = "height" Then gotheight = GetDoubleAttribute(attr.Value, newheight)
+                If attr.Name.LocalName = "viewBox" Then gotviewbox = GetRectAttribute(attr.Value, newviewbox)
+            Next
+
+            Clip = If(gotviewbox, New RectangleGeometry(newviewbox), Nothing)
+            Width = If(gotviewbox, newviewbox.Right, If(gotwidth, newwidth, Double.NaN))
+            Height = If(gotviewbox, newviewbox.Bottom, If(gotheight, newheight, Double.NaN))
+            ViewBox = If(gotviewbox, newviewbox, Nothing)
+            TranslateTransform.X = If(gotviewbox, -_viewbox.X, 0)
+            TranslateTransform.Y = If(gotviewbox, -_viewbox.Y, 0)
+            ScaleTransform.ScaleX = If(gotviewbox, _viewbox.Right / _viewbox.Width, 1)
+            ScaleTransform.ScaleY = If(gotviewbox, _viewbox.Bottom / _viewbox.Height, 1)
+        End If
+
+        If node.Name.LocalName = "path" Then
+            For Each attr In node.Attributes
+                If attr.Name.LocalName = "d" AndAlso Not String.IsNullOrEmpty(attr.Value) Then
+                    Try
+                        result.Children.Add(Geometry.Parse(attr.Value))
+                    Catch
+                    End Try
+                End If
+            Next
+        End If
+
+        If node.Name.LocalName = "line" Then
+            Dim x1, y1, x2, y2 As Double
+            If GetDoubleAttribute(node, "x1", x1) AndAlso
+                GetDoubleAttribute(node, "y1", y1) AndAlso
+                GetDoubleAttribute(node, "x2", x2) AndAlso
+                GetDoubleAttribute(node, "y2", y2) Then result.Children.Add(New LineGeometry(New Point(x1, y2), New Point(x2, y2)))
+        End If
+
+        If node.Name.LocalName = "rect" Then
+            Dim x, y, w, h As Double
+            If GetDoubleAttribute(node, "x", x) AndAlso
+                GetDoubleAttribute(node, "y", y) AndAlso
+                GetDoubleAttribute(node, "width", w) AndAlso
+                GetDoubleAttribute(node, "height", h) Then result.Children.Add(New RectangleGeometry(New Rect(x, y, w, h)))
+        End If
+
+        If node.Name.LocalName = "ellipse" Then
+            Dim cx, cy, rx, ry As Double
+            If GetDoubleAttribute(node, "cx", cx) AndAlso
+                GetDoubleAttribute(node, "cy", cy) AndAlso
+                GetDoubleAttribute(node, "rx", rx) AndAlso
+                GetDoubleAttribute(node, "ry", ry) Then result.Children.Add(New EllipseGeometry(New Point(cx, cy), rx, ry))
+        End If
+
+        If node.Name.LocalName = "circle" Then
+            Dim cx, cy, r As Double
+            If GetDoubleAttribute(node, "cx", cx) AndAlso
+                GetDoubleAttribute(node, "cy", cy) AndAlso
+                GetDoubleAttribute(node, "r", r) Then result.Children.Add(New EllipseGeometry(New Point(cx, cy), r, r))
+        End If
+
+        If node.Name.LocalName = "polyline" Then
+            Try
+                Dim polyline As New Polyline
+                Dim attr As XAttribute = node.Attribute("points")
+                If attr IsNot Nothing AndAlso Not String.IsNullOrEmpty(attr.Value) Then
+                    polyline.Points = PointCollection.Parse(attr.Value)
+                    polyline.Measure(ViewBox.Size)
+                    polyline.Arrange(ViewBox)
+                    result.Children.Add(polyline.RenderedGeometry)
+                End If
+            Catch
+            End Try
+        End If
+
+        If node.Name.LocalName = "polygon" Then
+            Try
+                Dim polygon As New Polygon
+                Dim attr As XAttribute = node.Attribute("points")
+                If attr IsNot Nothing AndAlso Not String.IsNullOrEmpty(attr.Value) Then
+                    polygon.Points = PointCollection.Parse(attr.Value)
+                    polygon.Measure(ViewBox.Size)
+                    polygon.Arrange(ViewBox)
+                    result.Children.Add(polygon.RenderedGeometry)
+                End If
+            Catch
+            End Try
+        End If
+
+        For Each child As XElement In node.Elements
+            result.Children.Add(ReadSvgNode(child))
+        Next
+
+        Return result
     End Function
 
-    Private Function GetRectAttribute(reader As XmlReader, attr As String, ByRef r As Rect) As Boolean
-        Dim strvalue As String = reader.GetAttribute(attr)
-        If String.IsNullOrEmpty(strvalue) Then Return False
-        strvalue = Regex.Replace(strvalue, "[^0-9 .-]", "")
+
+    Private Function GetDoubleAttribute(element As XElement, name As String, ByRef d As Double) As Boolean
+        Dim attr As XAttribute = element.Attribute(name)
+        If attr Is Nothing Then Return False
+        Dim value As String = attr.Value
+        If String.IsNullOrEmpty(value) Then Return False
+        value = Regex.Replace(value, "[^0-9.-]", "")
+        Return Double.TryParse(value, d)
+    End Function
+
+    Private Function GetDoubleAttribute(value As String, ByRef d As Double) As Boolean
+        If String.IsNullOrEmpty(value) Then Return False
+        value = Regex.Replace(value, "[^0-9.-]", "")
+        Return Double.TryParse(value, d)
+    End Function
+
+    Private Function GetRectAttribute(value As String, ByRef r As Rect) As Boolean
+        If String.IsNullOrEmpty(value) Then Return False
+        value = Regex.Replace(value, "[^0-9 .-]", "")
         Try
-            r = Rect.Parse(strvalue)
+            r = Rect.Parse(value)
             Return True
         Catch
             Return False
